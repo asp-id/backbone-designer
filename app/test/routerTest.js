eaa4f3a81a0734617267d6aac5207b951777c3c8
David Klassen <f0rk.tt@gmail.com>
2012-12-29 04:16:52 +0700

    DRY up tests

--- app/test/routerTest.js
+++ app/test/routerTest.js
@@ -4,6 +4,22 @@
  * @author David Klassen <f0rk.tt@gmail.com> (c) 2012
  */
 
+/**
+ * Helper function that rebinds routes and
+ * returns a spy for given route.
+ */
+function spyOnAction(router, action) {
+  var spy = spyOn(router, action)
+
+  // Backbone stores route callbacks in Backbone.history.handlers
+  // so we have to clear them and rebind routes every time we want
+  // to spy on route handler.
+  Backbone.history.handlers = []
+  router._bindRoutes()
+
+  return spy
+}
+
 describe('Router', function () {
   beforeEach(function () {
     var self = this
@@ -16,12 +32,6 @@ describe('Router', function () {
       // change hash to be sure that hashChange is
       // triggered when other navigate calls are made
       self.router.navigate('foo')
-
-      // Backbone stores route callbacks in
-      // Backbone.history.handlers array and
-      // we have to clear it and rebind routes
-      // every time we want to spy on route handler
-      Backbone.history.handlers = []
 
       done = true
     })
@@ -45,8 +55,7 @@ describe('Router', function () {
 
   describe('"" (root) route', function () {
     it('should call "designer" action', function () {
-      var spy = spyOn(this.router, 'designer')
-      this.router._bindRoutes()
+      var spy = spyOnAction(this.router, 'designer')
       this.router.navigate('', { trigger: true })
       expect(spy).toHaveBeenCalled()
     })
@@ -54,8 +63,7 @@ describe('Router', function () {
 
   describe('"editor" route', function () {
     it('should call "editor" action', function () {
-      var spy = spyOn(this.router, 'editor')
-      this.router._bindRoutes()
+      var spy = spyOnAction(this.router, 'editor')
       this.router.navigate('editor', { trigger: true })
       expect(spy).toHaveBeenCalled()
     })
@@ -63,8 +71,7 @@ describe('Router', function () {
 
   describe('"preview" route', function () {
     it('should call "preview" action', function () {
-      var spy = spyOn(this.router, 'preview')
-      this.router._bindRoutes()
+      var spy = spyOnAction(this.router, 'preview')
       this.router.navigate('preview', { trigger: true })
       expect(spy).toHaveBeenCalled()
     })
@@ -72,8 +79,7 @@ describe('Router', function () {
 
   describe('"notfound" action', function () {
     it('should be called for any undefined route', function () {
-      var spy = spyOn(this.router, 'notfound')
-      this.router._bindRoutes()
+      var spy = spyOnAction(this.router, 'notfound')
       this.router.navigate('nonexistentroute', { trigger: true })
       expect(spy.calls.length).toEqual(1)
       this.router.navigate('nonexistentroute2', { trigger: true })
@@ -81,8 +87,7 @@ describe('Router', function () {
     })
 
     it('should not be called for defined routes', function () {
-      var spy = spyOn(this.router, 'notfound')
-      this.router._bindRoutes()
+      var spy = spyOnAction(this.router, 'notfound')
       this.router.navigate('', { trigger: true })
       expect(spy.calls.length).toEqual(0)
     })
