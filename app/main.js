0e1f4c0dbe07b78ba4a89d2ffd5a03ce2acbb40c
David Klassen <f0rk.tt@gmail.com>
2012-12-29 05:01:03 +0700

    Added pushState support

--- app/main.js
+++ app/main.js
@@ -22,7 +22,43 @@ require.config({
 
 require(['jquery', 'backbone', 'router'], function ($, Backbone, router) {
   $(function () {
+    // Test for pushState support
+    var pushState = false
+    if (history.pushState) {
+      pushState = true
+      try {
+        history.replaceState(null, null, 'test')
+      } catch (e) {
+        pushState = false
+      }
+
+      if (pushState) {
+        history.replaceState(null, null, '/')
+      }
+    }
+
+    if (pushState) {
+      // Globally capture clicks. If they are internal and not in the pass
+      // through list, route them through Backbone's navigate method.
+      $(document).on('click', "a[href^='#']", function (e) {
+        href = $(e.currentTarget).attr('href')
+
+        // Allow shift+click for new tabs, etc.
+        if (!e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey) {
+          e.preventDefault()
+        }
+
+        // Remove leading slashes and hash bangs (backward compatibility)
+        url = href.replace(/^\//, '').replace('\#', '')
+
+        // Instruct Backbone to trigger routing events
+        router.navigate(url, { trigger: true })
+
+        return false
+      })
+    }
+
     // Start the routing and load the default route
-    Backbone.history.start()
+    Backbone.history.start({ pushState: pushState })
   })
 })
